8b4b38a723f9e152d0721778fa2d39ed7d9b610c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-03-16 12:13:24 +0300

    Fixing coding standards

--- src/webfiori/database/mysql/MySQLConnection.php
+++ src/webfiori/database/mysql/MySQLConnection.php
@@ -28,9 +28,7 @@ use mysqli;
 use mysqli_stmt;
 use webfiori\database\AbstractQuery;
 use webfiori\database\Connection;
-use webfiori\database\ConnectionInfo;
 use webfiori\database\ResultSet;
-use webfiori\database\DatabaseException;
 /**
  * A class that represents a connection to MySQL server.
  * 
@@ -44,200 +42,20 @@ use webfiori\database\DatabaseException;
 class MySQLConnection extends Connection {
     /**
      *
-     * @var mysqli_stmt|null
-     * 
-     * @since 1.0.2 
-     */
-    private $sqlStm;
-    /**
-     *
      * @var mysqli|null
      * 
      * @since 1.0 
      */
     private $link;
     /**
-     * Returns the instance at which the connection uses to execute 
-     * database queries.
+     *
+     * @var mysqli_stmt|null
      * 
-     * @return mysqli|null The object which is used to connect to the database.
-     * 
-     * @since 1.0.1
+     * @since 1.0.2 
      */
-    public function getMysqli() {
-        return $this->link;
-    }
+    private $sqlStm;
     public function __destruct() {
         mysqli_close($this->link);
-    }
-    /**
-     * Connect to MySQL database.
-     * 
-     * @return boolean If the connection was established, the method will return 
-     * true. If the attempt to connect fails, the method will return false.
-     * 
-     * @since 1.0
-     */
-    public function connect() {
-        $test = false;
-        $connInfo = $this->getConnectionInfo();
-        
-        $host = $connInfo->getHost();
-        $port = $connInfo->getPort();
-        $user = $connInfo->getUsername();
-        $pass = $connInfo->getPassword();
-        $dbName = $connInfo->getDBName();
-        
-        set_error_handler(function()
-        {
-        });
-        $this->link = mysqli_connect($host, $user, $pass, null, $port);
-        restore_error_handler();
-
-        if ($this->link instanceof mysqli) {
-            $test = mysqli_select_db($this->link, $dbName);
-            
-            if ($test) {
-                $this->link->set_charset("utf8");
-                mysqli_query($this->link, "set character_set_client='utf8'");
-                mysqli_query($this->link, "set character_set_results='utf8'");
-            }
-        } else {
-            $this->setErrCode(mysqli_connect_errno());
-            $this->setErrMessage(mysqli_connect_error());
-        }
-        
-        return $test;
-    }
-    
-    /**
-     * Execute MySQL query.
-     * 
-     * @param AbstractQuery $query A query builder that has the generated MySQL 
-     * query.
-     * 
-     * @return boolean If the query successfully executed, the method will return 
-     * true. Other than that, the method will return true.
-     * 
-     * @since 1.0
-     */
-    public function runQuery(AbstractQuery $query = null) {
-        $this->setLastQuery($query);
-
-        if ($query instanceof MySQLQuery && !$query->isBlobInsertOrUpdate()) {
-            $table = $query->getTable();
-            if ($table !== null) {
-                $collation = $query->getTable()->getCollation();
-            } else {
-                $collation = 'utf8mb4_unicode_520_ci';
-            }
-            $stm = mysqli_prepare($this->link, 'set collation_connection = ?');
-            $stm->bind_param('s', $collation);
-            $stm->execute();
-        }
-        $qType = $query->getLastQueryType();
-
-        if ($qType == 'insert' || $qType == 'update') {
-            return $this->_insertQuery();
-        } else if ($qType == 'select' || $qType == 'show'|| $qType == 'describe') {
-            return $this->_selectQuery();
-        } else {
-            return $this->_otherQuery();
-        }
-    }
-    private function _bindAndExc() {
-        $this->prepare();
-        $this->bind($this->getLastQuery()->getParams());
-        return $this->sqlStm->execute();
-    }
-    private function _insertQuery() {
-        $query = $this->getLastQuery();
-        if ($query->isPrepareBeforeExec()) {
-            $r = $this->_bindAndExc();
-        } else {
-            $r = mysqli_query($this->link, $query->getQuery());
-        }
-        $retVal = false;
-        
-        if (!$r) {
-            $this->setErrMessage($this->link->error);
-            $this->setErrCode($this->link->errno);
-
-            $r = mysqli_multi_query($this->link, $query->getQuery());
-
-            if ($r) {
-                $this->setErrMessage('NO ERRORS');
-                $this->setErrCode(0);
-
-                $retVal = true;
-            }
-        } else {
-            $retVal = true;
-        }
-        $query->setIsBlobInsertOrUpdate(false);
-
-        return $retVal;
-    }
-    private function _otherQuery() {
-        $query = $this->getLastQuery()->getQuery();
-        $retVal = false;
-        if ($this->getLastQuery()->isPrepareBeforeExec()) {
-            $r = $this->_bindAndExc();
-        } else {
-            if (!$this->getLastQuery()->isMultiQuery()) {
-                $r = mysqli_query($this->link, $query);
-            } else {
-                $r = mysqli_multi_query($this->link, $query);
-            }
-        }
-        if (!$r) {
-            $this->setErrMessage($this->link->error);
-            $this->setErrCode($this->link->errno);
-        } else {
-            $this->setErrMessage('NO ERRORS');
-            $this->setErrCode(0);
-            $this->getLastQuery()->setIsBlobInsertOrUpdate(false);
-            $retVal = true;
-        }
-        $r = mysqli_query($this->link, $query);
-        if ($r === true || gettype($r) == 'object') {
-            $retVal = true;
-        }
-
-        return $retVal;
-    }
-    private function _selectQuery() {
-        if ($this->getLastQuery()->isPrepareBeforeExec()) {
-            $r = $this->_bindAndExc();
-        } else {
-            $r = mysqli_query($this->link, $this->getLastQuery()->getQuery());
-        }
-
-        if ($r) {
-            $this->setErrCode(0);
-            $rows = [];
-            if ($this->getLastQuery()->isPrepareBeforeExec()) {
-                $this->sqlStm->store_result();
-                $r = $this->sqlStm->get_result();
-            }
-            if (function_exists('mysqli_fetch_all')) {
-                $rows = mysqli_fetch_all($r, MYSQLI_ASSOC);
-            } else {
-
-                while ($row = $r->fetch_assoc()) {
-                    $rows[] = $row;
-                }
-
-            }
-            $this->setResultSet(new ResultSet($rows));
-
-            return true;
-        } else {
-            $this->setErrMessage($this->link->error);
-            $this->setErrCode($this->link->errno);
-
-            return false;
-        }
     }
     /**
      * Bind parameters to MySQL query.
@@ -268,6 +86,56 @@ class MySQLConnection extends Connection {
         }
     }
     /**
+     * Connect to MySQL database.
+     * 
+     * @return boolean If the connection was established, the method will return 
+     * true. If the attempt to connect fails, the method will return false.
+     * 
+     * @since 1.0
+     */
+    public function connect() {
+        $test = false;
+        $connInfo = $this->getConnectionInfo();
+
+        $host = $connInfo->getHost();
+        $port = $connInfo->getPort();
+        $user = $connInfo->getUsername();
+        $pass = $connInfo->getPassword();
+        $dbName = $connInfo->getDBName();
+
+        set_error_handler(function()
+        {
+        });
+        $this->link = mysqli_connect($host, $user, $pass, null, $port);
+        restore_error_handler();
+
+        if ($this->link instanceof mysqli) {
+            $test = mysqli_select_db($this->link, $dbName);
+
+            if ($test) {
+                $this->link->set_charset("utf8");
+                mysqli_query($this->link, "set character_set_client='utf8'");
+                mysqli_query($this->link, "set character_set_results='utf8'");
+            }
+        } else {
+            $this->setErrCode(mysqli_connect_errno());
+            $this->setErrMessage(mysqli_connect_error());
+        }
+
+        return $test;
+    }
+    /**
+     * Returns the instance at which the connection uses to execute 
+     * database queries.
+     * 
+     * @return mysqli|null The object which is used to connect to the database.
+     * 
+     * @since 1.0.1
+     */
+    public function getMysqli() {
+        return $this->link;
+    }
+    /**
      * Prepare SQL statement.
      * 
      * @return boolean If the statement was successfully prepared, the method 
@@ -277,14 +145,152 @@ class MySQLConnection extends Connection {
      */
     public function prepare() {
         $queryObj = $this->getLastQuery();
+
         if ($queryObj !== null) {
             $queryStr = $queryObj->getQuery();
-            $this->sqlStm = mysqli_prepare ($this->link, $queryStr);
+            $this->sqlStm = mysqli_prepare($this->link, $queryStr);
+
             if (gettype($this->sqlStm) == 'object') {
                 return true;
             }
         }
+
         return false;
     }
 
+    /**
+     * Execute MySQL query.
+     * 
+     * @param AbstractQuery $query A query builder that has the generated MySQL 
+     * query.
+     * 
+     * @return boolean If the query successfully executed, the method will return 
+     * true. Other than that, the method will return true.
+     * 
+     * @since 1.0
+     */
+    public function runQuery(AbstractQuery $query = null) {
+        $this->setLastQuery($query);
+
+        if ($query instanceof MySQLQuery && !$query->isBlobInsertOrUpdate()) {
+            $table = $query->getTable();
+
+            if ($table !== null) {
+                $collation = $query->getTable()->getCollation();
+            } else {
+                $collation = 'utf8mb4_unicode_520_ci';
+            }
+            $stm = mysqli_prepare($this->link, 'set collation_connection = ?');
+            $stm->bind_param('s', $collation);
+            $stm->execute();
+        }
+        $qType = $query->getLastQueryType();
+
+        if ($qType == 'insert' || $qType == 'update') {
+            return $this->_insertQuery();
+        } else if ($qType == 'select' || $qType == 'show'|| $qType == 'describe') {
+            return $this->_selectQuery();
+        } else {
+            return $this->_otherQuery();
+        }
+    }
+    private function _bindAndExc() {
+        $this->prepare();
+        $this->bind($this->getLastQuery()->getParams());
+
+        return $this->sqlStm->execute();
+    }
+    private function _insertQuery() {
+        $query = $this->getLastQuery();
+
+        if ($query->isPrepareBeforeExec()) {
+            $r = $this->_bindAndExc();
+        } else {
+            $r = mysqli_query($this->link, $query->getQuery());
+        }
+        $retVal = false;
+
+        if (!$r) {
+            $this->setErrMessage($this->link->error);
+            $this->setErrCode($this->link->errno);
+
+            $r = mysqli_multi_query($this->link, $query->getQuery());
+
+            if ($r) {
+                $this->setErrMessage('NO ERRORS');
+                $this->setErrCode(0);
+
+                $retVal = true;
+            }
+        } else {
+            $retVal = true;
+        }
+        $query->setIsBlobInsertOrUpdate(false);
+
+        return $retVal;
+    }
+    private function _otherQuery() {
+        $query = $this->getLastQuery()->getQuery();
+        $retVal = false;
+
+        if ($this->getLastQuery()->isPrepareBeforeExec()) {
+            $r = $this->_bindAndExc();
+        } else {
+            if (!$this->getLastQuery()->isMultiQuery()) {
+                $r = mysqli_query($this->link, $query);
+            } else {
+                $r = mysqli_multi_query($this->link, $query);
+            }
+        }
+
+        if (!$r) {
+            $this->setErrMessage($this->link->error);
+            $this->setErrCode($this->link->errno);
+        } else {
+            $this->setErrMessage('NO ERRORS');
+            $this->setErrCode(0);
+            $this->getLastQuery()->setIsBlobInsertOrUpdate(false);
+            $retVal = true;
+        }
+        $r = mysqli_query($this->link, $query);
+
+        if ($r === true || gettype($r) == 'object') {
+            $retVal = true;
+        }
+
+        return $retVal;
+    }
+    private function _selectQuery() {
+        if ($this->getLastQuery()->isPrepareBeforeExec()) {
+            $r = $this->_bindAndExc();
+        } else {
+            $r = mysqli_query($this->link, $this->getLastQuery()->getQuery());
+        }
+
+        if ($r) {
+            $this->setErrCode(0);
+            $rows = [];
+
+            if ($this->getLastQuery()->isPrepareBeforeExec()) {
+                $this->sqlStm->store_result();
+                $r = $this->sqlStm->get_result();
+            }
+
+            if (function_exists('mysqli_fetch_all')) {
+                $rows = mysqli_fetch_all($r, MYSQLI_ASSOC);
+            } else {
+                while ($row = $r->fetch_assoc()) {
+                    $rows[] = $row;
+                }
+            }
+            $this->setResultSet(new ResultSet($rows));
+
+            return true;
+        } else {
+            $this->setErrMessage($this->link->error);
+            $this->setErrCode($this->link->errno);
+
+            return false;
+        }
+    }
 }
