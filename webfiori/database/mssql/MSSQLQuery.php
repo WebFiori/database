7d1f5e73c38e68f44de32f51fa70cb6696f5ccf6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-10-12 00:19:35 +0300

    Update MSSQLQuery.php

--- webfiori/database/mssql/MSSQLQuery.php
+++ webfiori/database/mssql/MSSQLQuery.php
@@ -10,7 +10,33 @@ use webfiori\database\AbstractQuery;
  * @version 1.0
  */
 class MSSQLQuery extends AbstractQuery {
-    //put your code here
+    /**
+     * Adds a square brackets around a string.
+     * 
+     * @param string $str This can be the name of a column in a table or the name 
+     * of a table.
+     * 
+     * @return string|null The method will return a string surrounded by square 
+     * brackets. 
+     * If empty string is given, the method will return null.
+     * 
+     * @since 1.0
+     */
+    public static function squareBr($str) {
+        $trimmed = trim($str);
+
+        if (strlen($trimmed) != 0) {
+            $exp = explode('.', $trimmed);
+
+            $arr = [];
+
+            foreach ($exp as $xStr) {
+                $arr[] = '['.trim(trim($xStr, '['),']').']';
+            }
+
+            return implode('.', $arr);
+        }
+    }
     public function addCol($colKey, $location = null) {
         
         return $this;
