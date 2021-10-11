b1ef5fad53bee0ab69e85d2b7937ed446caf597c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-10-12 00:19:29 +0300

    Update MSSQLColumn.php

--- webfiori/database/mssql/MSSQLColumn.php
+++ webfiori/database/mssql/MSSQLColumn.php
@@ -26,8 +26,70 @@ class MSSQLColumn extends Column {
             'money',
             'bit',
             'decimal',
-            'float'
+            'float',
+            'boolean'
         ]);
+    }
+    public function __toString() {
+        $retVal = $this->_firstColPart();
+        $retVal .= $this->_nullPart();
+        $retVal .= $this->_defaultPart();
+        return $retVal;
+    }
+    private function _defaultPart() {
+        $colDataType = $this->getDatatype();
+        $colDefault = $this->getDefault();
+
+        if ($colDefault !== null) {
+            if ($colDataType == 'boolean') {
+                if ($this->getDefault() === true) {
+                    return 'default 1 ';
+                } else {
+                    return 'default 0 ';
+                }
+            } else if ($colDataType == 'datetime2' || $colDataType == 'time'
+                    || $colDataType == 'date') {
+                if ($colDefault == 'now' || $colDefault == 'current_timestamp') {
+                    return 'default getdate() ';
+                } else {
+                    return 'default '.$this->cleanValue($colDefault).' ';
+                }
+            } else {
+                return 'default '.$this->cleanValue($colDefault).' ';
+            }
+        }
+    }
+    private function _nullPart() {
+        $colDataType = $this->getDatatype();
+
+        if (!$this->isNull() || $colDataType == 'boolean') {
+            return 'not null ';
+        } else {
+            return 'null ';
+        }
+    }
+    
+    private function _firstColPart() {
+        $retVal = MSSQLQuery::squareBr($this->getName()).' ';
+        $colDataType = MSSQLQuery::squareBr($this->getDatatype());
+        
+        if ($colDataType == 'varchar' || $colDataType == 'nvarchar'
+                || $colDataType == 'char' || $colDataType == 'nchar'
+                || $colDataType == 'binary' || $colDataType == 'varbinary') {
+            $retVal .= $colDataType.'('.$this->getSize().') ';
+        } else if ($colDataType == 'boolean') {
+            $retVal .= 'bit(1) ';
+        } else if ($colDataType == 'decimal') {
+            if ($this->getSize() != 0) {
+                $retVal .= $colDataType.'('.$this->getSize().','.$this->getScale().') ';
+            } else {
+                $retVal .= $colDataType.'(18,0) ';
+            }
+        } else {
+            $retVal .= $colDataType.' ';
+        }
+
+        return $retVal;
     }
     public function asString() {
         
