0bcfec09d827e37444e714ef9f7c4b71c0b118d1
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-04-03 23:00:40 +0300

    Update MySQLConnection.php

--- webfiori/database/mysql/MySQLConnection.php
+++ webfiori/database/mysql/MySQLConnection.php
@@ -82,7 +82,7 @@ class MySQLConnection extends Connection {
         if (!function_exists('mysqli_connect')) {
             throw new DatabaseException('mysqli extension is missing.');
         }
-        set_error_handler(null);
+        set_error_handler(function() {});
         $this->link = mysqli_connect($host, $user, $pass, null, $port);
         restore_error_handler();
 
