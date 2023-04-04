5d78040e4e5f7e496eba61e4f12181294b9152ec
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-04-05 00:49:47 +0300

    Update connect-to-db.php

--- samples/connect-to-db.php
+++ samples/connect-to-db.php
@@ -4,12 +4,9 @@ use webfiori\database\ConnectionInfo;
 use webfiori\database\Database;
 use webfiori\database\DatabaseException;
 
-try {
-    //This assumes that MySQL is installed on locahost
-    //and root password is set to '123456' 
-    //and there is a schema with name 'testing_db'
-    $connection = new ConnectionInfo('mysql', 'root', '123456', 'testing_db');
-    $database = new Database($connection);
-} catch (DatabaseException $ex) {
-    die($ex->getCode().' - '.$ex->getMessage());
-}
+//This assumes that MySQL is installed on locahost
+//and root password is set to '123456' 
+//and there is a schema with name 'testing_db'
+$connection = new ConnectionInfo('mysql', 'root', '123456', 'testing_db');
+$database = new Database($connection);
+
