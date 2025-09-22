e119556963bb1f9765f82872f8a3b76f2deb0f96
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-09-22 19:09:02 +0300

    test: Fix Test Cases

--- tests/WebFiori/Tests/Database/Schema/AbstractMigrationTest.php
+++ tests/WebFiori/Tests/Database/Schema/AbstractMigrationTest.php
@@ -11,7 +11,8 @@ class AbstractMigrationTest extends TestCase {
     public function testGetType() {
         $migration = new class extends AbstractMigration {
             public function execute(Database $db): void {}
-            public function rollback(Database $db): void {}
+            public function up(Database $db): bool { return true; }
+            public function down(Database $db): bool { return true; }            public function rollback(Database $db): void {}
         };
         
         $this->assertEquals('migration', $migration->getType());
